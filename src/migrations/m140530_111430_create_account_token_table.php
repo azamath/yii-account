5c865e569d9b65371bc45cbf74335c600e282982
Azamat Holmeer <azamat.holmeer@gmail.com>
2014-07-04 15:35:40 +0500

    Adding back crisu83/yiistrap. Table account_token has too long unique key. Modified column type and unique key fields.

--- src/migrations/m140530_111430_create_account_token_table.php
+++ src/migrations/m140530_111430_create_account_token_table.php
@@ -9,11 +9,11 @@ class m140530_111430_create_account_token_table extends CDbMigration
             array(
                 'id' => 'pk',
                 'accountId' => 'int NOT NULL',
-                'type' => 'string NOT NULL',
+                'type' => 'varchar(32) NOT NULL',
                 'token' => 'string NOT NULL',
                 'createdAt' => 'timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP',
                 'status' => "integer NOT NULL DEFAULT '0'",
-                'UNIQUE KEY accountId_type_token (accountId, type, token)',
+                'UNIQUE KEY accountId_type_token (accountId, type)',
             )
         );
 	}
